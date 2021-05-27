e0f112d34a3e5db308a549cc6178d0ff21e0adb3
Szymon Ryś <rysszymon00@gmail.com>
2021-05-27 21:00:51 +0200

    try open on linux

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## How to install pyaudoi/ portaudio?
 Solution working on Linux Mint:
 https://stackoverflow.com/questions/20023131/cannot-install-pyaudio-gcc-error
+\+ possibly: `sudo apt-get install python-pyaudio python3-pyaudio `
+
 Solution for Windows:
 https://stackoverflow.com/questions/52283840/i-cant-install-pyaudio-on-windows-how-to-solve-error-microsoft-visual-c-14
--- speech_recog.py
+++ speech_recog.py
@@ -2,9 +2,11 @@ import speech_recognition as sr
 
 listener = sr.Recognizer()
 print(listener)
+print(sr.Microphone().list_microphone_names())
 try:
     print("listening...")
-    with sr.Microphone() as source:
+    # print(sr.Microphone().list_microphone_names())
+    with sr.Microphone(device_index=2) as source:
         voice = listener.listen(source)
         print("err1")
         info = listener.recognize_google(voice)
