6400a94b52ce105fa86e3d7df9eb5f87b2e2fbac
mrrys00 <rysszymon00@gmail.com>
2021-06-02 17:10:04 +0200

    add some changes

--- config_file.py
+++ config_file.py
@@ -1,3 +1,3 @@
-input_wav_file_path = "wav_samples/test.wav"
-output_file_path = "shitty.txt"
+input_wav_file_path = "wav_samples/listening-test_v3.wav"
+output_file_path = "shitty_v3.txt"
 logs = True
